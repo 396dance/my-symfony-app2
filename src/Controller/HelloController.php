b5ffd5e961e5b699b700ee3bf119a9b67fe85cab
Mikuno Yasuhiro <396dance@gmail.com>
2025-05-25 22:38:29 +0900

    P45まで完了

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,16 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}/{path}", name="hello")
+     * @Route("/hello", name="hello")
      */
-    public function index($name='noname',$path='no password')
+    public function index(Request $request)
     {
+        $name = $request->get('name');
+        $path = $request->get('pass');
         $result = '<html><body><ol>';
         $result .= '<h1>Parameter</h1>';
         $result .= '<p>name: '. $name.'</p>';
