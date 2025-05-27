0daf8bf8f0067cb64256e845ce9e000acc9ded29
Mikuno Yasuhiro <396dance@gmail.com>
2025-05-27 23:39:28 +0900

    テンプレートエンジンの実装方法の確認

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -21,23 +21,6 @@ class HelloController extends AbstractController
      */
     public function index(Request $request, LoggerInterface $logger)
     {
-        $data = array(
-            'name' => array('first' => 'Taro', 'second' => 'Yamada'),
-            'age' => 36, 'mail' => 'taro@yamada.kun' 
-        );
-        $logger->info(serialize($data));
-        return new JsonResponse($data);
-    }
-
-    /**
-     * @Route("/other/{domain}", name="other")
-     */
-    public function other(Request $request, $domain="")
-    {
-        if ($domain == "") {
-            return $this->redirect('/hello');
-        } else {
-            return new RedirectResponse("http://{$domain}.com");
-        };
+        return $this->render('hello/index.html.twig',['controller_name' => 'HelloController',]);
     }
 }
