dd11efe1b462a3caa4ba839414b60b047d7bf05c
Mikuno Yasuhiro <396dance@gmail.com>
2025-05-27 21:35:34 +0900

    ログの出力方法の確認

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,56 +19,25 @@ class HelloController extends AbstractController
     /**
      * @Route("/hello", name="hello")
      */
-    public function index(Request $request)
+    public function index(Request $request, LoggerInterface $logger)
     {
-        $content = <<< EOM
-        <html><head><title>Hello</title><head>
-        <body><h1>Hello!<h1>
-        <p>this is Symfony sample page.</p>
-        </body></html>
-EOM;
-        $response = new Response(
-            $content,
-            Response::HTTP_OK,
-            array('content-type' => 'text/html')
+        $data = array(
+            'name' => array('first' => 'Taro', 'second' => 'Yamada'),
+            'age' => 36, 'mail' => 'taro@yamada.kun' 
         );
-
-        return $response;
+        $logger->info(serialize($data));
+        return new JsonResponse($data);
     }
 
     /**
-     * @Route("/notfound", name="notfound")
+     * @Route("/other/{domain}", name="other")
      */
-    public function notfound(Request $request)
+    public function other(Request $request, $domain="")
     {
-        $content = <<< EOM
-        <html><head><title>ERROR</title><head>
-        <body><h1>ERROR 404!<h1>
-        </body></html>
-EOM;
-        $response = new Response(
-            $content,
-            Response::HTTP_NOT_FOUND,
-            array('content-type' => 'text/html')
-        );
-        return $response;
-    }
-
-    /**
-     * @Route("/error", name="error")
-     */
-    public function error(Request $request)
-    {
-        $content = <<< EOM
-        <html><head><title>ERROR</title><head>
-        <body><h1>ERROR 500!<h1>
-        </body></html>
-EOM;
-        $response = new Response(
-            $content,
-            Response::HTTP_INTERNAL_SERVER_ERROR,
-            array('content-type' => 'text/html')
-        );
-        return $response;
+        if ($domain == "") {
+            return $this->redirect('/hello');
+        } else {
+            return new RedirectResponse("http://{$domain}.com");
+        };
     }
 }
